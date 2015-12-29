cae3fce312040e8022f36316930ec1684a9e6426
Epli <olafurkon@gmail.com>
2015-12-29 21:31:25 +0000

    Changed Jump logic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,12 +3,45 @@ using UnityEngine;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-	public float maxspeed = 10f;
-	public float jumpForce = 0.05f;
-	public float jumpX = 0;
-	public float doubleJumpModifier = 1.2f;
 
-	private float groundRadius = 1f;
+	public class GroundState {
+		private GameObject body;
+		private float width, height, length;
+
+		public GroundState(GameObject b) {
+			body = b;
+		}
+
+		public bool isWall() {
+			return false;
+		}
+
+		public bool isGround() {
+			return true;
+		}
+
+		public bool isTouching() {
+			return true;
+		}
+			
+		// Returns direction of wall	
+		public int wallDirection() {
+			return -1;
+		}
+	}
+
+	public float speed    	  = 14f;
+	public float accel    	  = 6f;
+	public float airAccel 	  = 3f;
+	public float jump     	  = 14f;
+	public float maxJumpFrame = 25f;
+	
+	private bool isJumping = true;
+
+	private GroundState state;
+	private Vector2 input;
+
+	public float maxspeed = 10f;
 
 	public Transform groundCheck;
 	public LayerMask whatIsGround;
@@ -19,73 +52,67 @@ public class PlayerController : MonoBehaviour {
 
 	[HideInInspector]
 	public  bool lookingRight = true;
-	private bool doubleJump = false;
-	private bool standing = true;
 
 	// Use this for initialization
 	void Start () {  
 		rb2d = GetComponent<Rigidbody2D>();
 		anim = GetComponent<Animator>();
 		sprite = GetComponent<SpriteRenderer> ();
+
+		state = new GroundState (transform.gameObject);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButton("Jump") && standing) {
-			//rb2d.velocity = new Vector2(rb2d.velocity.x, -jumpForce);
-			//jumpX = 0.2f;
+		if      (Input.GetKey (KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))  input.x = -1;
+		else if (Input.GetKey (KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) input.x = 1;
+		else                                        						   input.x = 0;
 
-			standing = false;
-			rb2d.AddForce(transform.up * 30000f * Time.deltaTime, ForceMode2D.Force);
-		}
+		if (Input.GetKey (KeyCode.Space) || Input.GetKey (KeyCode.W)) {
+			if(isJumping) {
+				input.y += 1;
+				isJumping = input.y <= maxJumpFrame;
+			} else input.y = 0;
 
-		if (Input.GetButtonDown ("Jump")) {
-			//rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce * 0.1f * jumpX);
-			//jumpX = ((jumpX > 0.1f) ? jumpX * 0.95f : 0f);
-		}
-
-		Debug.Log (rb2d.velocity.y);
+			Debug.Log ("Acceleration Y: " + isJumping + ", mY: " + input.y);
+		} else 																     
+			input.y = 0;
 	}
-	
+
+
 	void FixedUpdate()
 	{
-		// grounded = setStatus ();
-
 		float move = Input.GetAxis ("Horizontal");
 	
-		// Move
-		rb2d.velocity = new Vector2(move * maxspeed, rb2d.velocity.y);
+		// Force
+		rb2d.AddForce(  new Vector2(((input.x * speed) - rb2d.velocity.x) * (state.isGround() ? accel : airAccel), 0));
+
+		// Velocity
+		rb2d.velocity = new Vector2(
+			(input.x == 0 && state.isGround())   ? 0              : rb2d.velocity.x, 
+			(input.y >= 1 && state.isTouching()) ? jump + (input.y / 100f) : rb2d.velocity.y);
+
+		// Wall Jump
+		if (state.isWall () && !state.isGround () && input.y == 1)
+			rb2d.velocity = new Vector2 (-state.wallDirection () * speed * 0.75f, 
+			                            rb2d.velocity.y);
+
+		//Debug.Log ("vX: " + rb2d.velocity.x + ", vY: " + rb2d.velocity.y);
+		//Debug.Log ("mX: " + input.x + ", mY: " + input.y);
 
 		// Flip player
-		if ((move > 0 && !lookingRight) || (move < 0 && lookingRight)) {
-			Flip ();
-		} else {
-			anim.SetBool ("Run", rb2d.velocity.x != 0.0);
-		}
+		if ((move > 0 && !lookingRight) || (move < 0 && lookingRight)) Flip();
+		else anim.SetBool ("Run", rb2d.velocity.x != 0.0);
 	}
-
-	private bool setStatus()
-	{
-		float yVelocity = rb2d.velocity.y;
-
-
-		anim.SetBool("Jump",   yVelocity >= 0);
-		anim.SetBool("Falling", yVelocity < 0);
-
-		Debug.Log ("Vertical: " + rb2d.velocity.y);
-
-		return false;
-	}	
 
 	public void OnCollisionEnter2D(Collision2D collision) {
 		if (collision.gameObject.tag == "Floor") {
+			isJumping = true;
 			anim.SetBool ("Jump", false);
-			standing = true;
 		}
 	}
 	
-	public void Flip()
-	{
+	public void Flip() {
 		lookingRight = !lookingRight;
 		Vector3 myScale = transform.localScale;
 		myScale.x *= -1;
