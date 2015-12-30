0148bc28296086eecd415d79028b2afd6e6591d7
Tornus <sonyz567@gmail.com>
2015-12-30 00:09:05 +0000

    Jumping curve
    Whole bunch of shit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -36,7 +36,8 @@ public class PlayerController : MonoBehaviour {
 	public float jump     	  = 14f;
 	public float maxJumpFrame = 25f;
 	
-	private bool isJumping = true;
+	private bool  isJumping = true;
+	private float force;
 
 	private GroundState state;
 	private Vector2 input;
@@ -55,11 +56,11 @@ public class PlayerController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {  
-		rb2d = GetComponent<Rigidbody2D>();
-		anim = GetComponent<Animator>();
+		rb2d   = GetComponent<Rigidbody2D>();
+		anim   = GetComponent<Animator>();
 		sprite = GetComponent<SpriteRenderer> ();
 
-		state = new GroundState (transform.gameObject);
+		state  = new GroundState (transform.gameObject);
 	}
 	
 	// Update is called once per frame
@@ -69,28 +70,28 @@ public class PlayerController : MonoBehaviour {
 		else                                        						   input.x = 0;
 
 		if (Input.GetKey (KeyCode.Space) || Input.GetKey (KeyCode.W)) {
-			if(isJumping) {
+			if (isJumping) {
 				input.y += 1;
+				force = Mathf.Cos (((input.y * Mathf.PI) / 180));
 				isJumping = input.y <= maxJumpFrame;
 			} else input.y = 0;
 
-			Debug.Log ("Acceleration Y: " + isJumping + ", mY: " + input.y);
-		} else 																     
-			input.y = 0;
+			Debug.Log ("Acceleration Y: " + isJumping + ", force: " + force + ", mY: " + input.y);
+		} else input.y = 0;
 	}
 
 
 	void FixedUpdate()
 	{
 		float move = Input.GetAxis ("Horizontal");
-	
+
 		// Force
 		rb2d.AddForce(  new Vector2(((input.x * speed) - rb2d.velocity.x) * (state.isGround() ? accel : airAccel), 0));
 
 		// Velocity
 		rb2d.velocity = new Vector2(
-			(input.x == 0 && state.isGround())   ? 0              : rb2d.velocity.x, 
-			(input.y >= 1 && state.isTouching()) ? jump + (input.y / 100f) : rb2d.velocity.y);
+			(input.x == 0 && state.isGround())   ? 0                       : rb2d.velocity.x, 
+			(input.y >= 1 && state.isTouching()) ? jump + (force / 100f)   : rb2d.velocity.y);
 
 		// Wall Jump
 		if (state.isWall () && !state.isGround () && input.y == 1)
